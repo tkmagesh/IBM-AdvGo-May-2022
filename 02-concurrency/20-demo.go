78eba4bb4aa72ac8e4a3e0f1c5e84d942c005b10
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-16 15:24:43 +0530

    singaling using channels

---
+++ 02-concurrency/20-demo.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("main started")
+
+	ch := genPrimes(3, 7*time.Second)
+	/* get the prime numbers and print them */
+	for primeNo := range ch {
+		fmt.Println("Prime No :", primeNo)
+	}
+
+	fmt.Println("main completed")
+}
+
+func genPrimes(start int, d time.Duration) <-chan int {
+	/* keep generating the prime numbers starting from  "start" for the duration "d' */
+	ch := make(chan int)
+	timeOutCh := time.After(d)
+	no := start
+	go func() {
+
+	LOOP:
+		for {
+			if !isPrime(no) {
+				no++
+				select {
+				case <-timeOutCh:
+					break LOOP
+				default:
+					continue LOOP
+				}
+			}
+			select {
+			case ch <- no:
+				time.Sleep(500 * time.Millisecond)
+				no++
+			case <-timeOutCh:
+				break LOOP
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+//our own implementation of time.After()
+func timeout(d time.Duration) <-chan time.Time {
+	ch := make(chan time.Time)
+	go func() {
+		time.Sleep(d)
+		ch <- time.Now()
+	}()
+	return ch
+}
---
+++ 02-concurrency/21-demo.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("main started")
+	stop := make(chan bool)
+
+	go func(d time.Duration) {
+		timeout := time.After(d)
+	LOOP:
+		for {
+			select {
+			case <-timeout:
+				stop <- true
+				break LOOP
+			}
+		}
+	}(7 * time.Second)
+
+	ch := genPrimes(3, stop)
+	/* get the prime numbers and print them */
+	for primeNo := range ch {
+		fmt.Println("Prime No :", primeNo)
+	}
+
+	fmt.Println("main completed")
+}
+
+func genPrimes(start int, stop <-chan bool) <-chan int {
+	/* keep generating the prime numbers starting from  "start" for the duration "d' */
+	ch := make(chan int)
+	no := start
+	go func() {
+	LOOP:
+		for {
+			if !isPrime(no) {
+				no++
+				select {
+				case <-stop:
+					break LOOP
+				default:
+					continue LOOP
+				}
+			}
+			select {
+			case ch <- no:
+				time.Sleep(500 * time.Millisecond)
+				no++
+			case <-stop:
+				break LOOP
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+//our own implementation of time.After()
+func timeout(d time.Duration) <-chan time.Time {
+	ch := make(chan time.Time)
+	go func() {
+		time.Sleep(d)
+		ch <- time.Now()
+	}()
+	return ch
+}
