e7dc78077818a3d0b5dd66f09646525999c2df1f
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-16 13:00:46 +0530

    updates

--- 02-concurrency/11-demo.go
+++ 02-concurrency/11-demo.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
+
+//09-demo.go but without waitGroup
 
 func main() {
 	//share memory by communicating
 	ch := make(chan int)
-	wg := &sync.WaitGroup{}
 	fmt.Println("main started")
-	wg.Add(1)
-	go add(100, 200, wg, ch)
+
+	go add(100, 200, ch)
 	result := <-ch
-	wg.Wait()
+
 	fmt.Println("result =", result)
 	fmt.Println("main completed")
 }
 
-func add(x, y int, wg *sync.WaitGroup, ch chan int) {
+func add(x, y int, ch chan int) {
 	result := x + y
 	ch <- result
-	wg.Done()
 }
